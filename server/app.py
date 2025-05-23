79abb85d618a33d7c480d28dcbb92cb90c26e7b1
pavan953 <pk5684865@gmail.com>
2025-05-23 16:37:18 +0530

    Refactor server to use HTTPServer and implement studio data retrieval

--- server/app.py
+++ server/app.py
@@ -1,38 +1,54 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
 
-app = Flask(__name__)
-CORS(app)
-
-# Dummy studio data
-studios = [
-    {
-        "name": "Epic Designs",
-        "ratingImage": 1,
-        "description": "Passionate team of 4 designers working out of Bangalore with an experience of 4 years.",
-        "projects": 57,
-        "years": 8,
-        "price": "$$",
-        "phone1": "+91 - 984532853",
-        "phone2": "+91 - 984532854",
-        "shortlistIcon": 1
-    },
-    {
-        "name": "Studio - D3",
-        "ratingImage": 2,
-        "description": "Passionate team of 4 designers working out of Bangalore with an experience of 4 years.",
-        "projects": 43,
-        "years": 6,
-        "price": "$$$",
-        "phone1": "+91 - 984532853",
-        "phone2": "+91 - 984532854",
-        "shortlistIcon": 2
-    },
-]
-
-@app.route("/api/studios", methods=["GET"])
-def get_studios():
-    return jsonify(studios)
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/api/studios":
+            studios = [
+                {
+                    "name": "Epic Designs",
+                    "ratingImage": 1,
+                    "description": "Passionate team of 4 designers working out of Bangalore with an experience of 4 years.",
+                    "projects": 57,
+                    "years": 8,
+                    "price": "$$",
+                    "phone1": "+91 - 984532853",
+                    "phone2": "+91 - 984532854",
+                    "shortlistIcon": 1
+                },
+                {
+                    "name": "Studio - D3",
+                    "ratingImage": 2,
+                    "description": "Passionate team of 4 designers working out of Bangalore with an experience of 4 years.",
+                    "projects": 43,
+                    "years": 6,
+                    "price": "$$$",
+                    "phone1": "+91 - 984532853",
+                    "phone2": "+91 - 984532854",
+                    "shortlistIcon": 2
+                },
+                {
+                    "name": "House of designs",
+                    "ratingImage": 1,
+                    "description": "New studio with fresh concepts from Delhi.",
+                    "projects": 18,
+                    "years": 2,
+                    "price": "$",
+                    "phone1": "+91 - 984532853",
+                    "phone2": "+91 - 984532854",
+                    "shortlistIcon": 1
+                }
+            ]
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(studios).encode('utf-8'))
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b"Not found")
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    server = HTTPServer(('localhost', 5000), handler)
+    print("Server running on http://localhost:5000")
+    server.serve_forever()
